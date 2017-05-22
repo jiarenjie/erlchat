9df1308062aa3344599fdc499a9729a052cf5e6c
jiarenjie <164977873@qq.com>
2017-05-22 17:43:11 +0800

    sss

--- src/erlchat.erl
+++ src/erlchat.erl
@@ -11,45 +11,101 @@
 -include("include/wechat.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-%% API
--export([get_code_url/3, get_access_token/3, refresh_token/2, get_userinfo/2, check/4, send_template_message/2]).
+-behaviour(gen_server).
+-record(state, {}).
+-define(SERVER, ?MODULE).
 
-get_code_url(Appid,Redirect_uri,State) when is_list(Appid),is_list(Redirect_uri),is_list(State) ->
-  Enc_url=edoc_lib:escape_uri(Redirect_uri),
-  Url = ?API_URL_CODE++"?appid="++Appid++"&redirect_uri="++Enc_url++"&response_type=code&scope=snsapi_userinfo&state=" ++ State ++"#wechat_redirect",
+%% API
+-export([get_code_url/0
+  , get_access_token/0
+  , get_access_token/1
+  , refresh_token/1
+  , get_userinfo/1
+  , verify/4
+  , send_template_message/2 ]).
+
+-export([init/1
+  , handle_call/3
+  , handle_cast/2
+  , handle_info/2
+  , terminate/2
+  , code_change/3]).
+
+-export([start_link/0]).
+
+
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init([]) ->
+  {ok, #state{}, 0}.
+handle_call(_Request, _From, State) ->
+  {reply, ok, State}.
+
+handle_cast(_Request, State) ->
+  {noreply, State}.
+
+handle_info(timeout, State) ->
+  Access_token = get_access_token(),
+  io:format("Access_token:~p~n",[Access_token]),
+  ok = application:set_env(erlchat, access_token, Access_token),
+  {noreply, State, 7000000}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+
+get_code_url() ->
+  Appid = get_apppid(),
+  Rerirect_url=get_redirect_uri(),
+  Enc_url=edoc_lib:escape_uri(Rerirect_url),
+  <<A:32,_B:32,_C:32>> = crypto:strong_rand_bytes(12),
+  State = integer_to_list(A),
+  Url = ?API_URL_CODE ++ "?appid=" ++ Appid ++ "&redirect_uri=" ++ Enc_url ++ "&response_type=code&scope=snsapi_userinfo&state=" ++ State ++ "#wechat_redirect",
   Url.
-get_access_token(Appid,Secret,Code) when is_list(Appid),is_list(Secret),is_binary(Code) ->
-  get_access_token(Appid,Secret,binary_to_list(Code));
-get_access_token(Appid,Secret,Code) when is_list(Appid),is_list(Secret),is_list(Code)->
-  %Access_token_Rui = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="++Appid++"&secret="++Secret++"&code="++binary_to_list(Code)++"&grant_type=authorization_code",
-  Access_token_Rui = ?API_URL_ACCESS_TOKEN++"?appid="++Appid++"&secret="++Secret++"&code="++Code++"&grant_type=authorization_code",
-  Response= wechat_util:http_get(Access_token_Rui),
-  Response;
-get_access_token(app,Appid,Secret) when is_list(Appid),is_list(Secret) ->
+get_access_token()->
+  Appid = get_apppid(),
+  Secret=get_secret(),
   %Access_token_Rui = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="++Appid++"&secret="++Secret++"&code="++binary_to_list(Code)++"&grant_type=authorization_code",
   Access_token_Rui = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&"++"appid="++Appid++"&secret="++Secret,
   Response= wechat_util:http_get(Access_token_Rui),
-  Response.
-refresh_token(Appid,REFRESH_TOKEN) when is_list(Appid),is_binary(REFRESH_TOKEN)->
-  refresh_token(Appid,binary_to_list(REFRESH_TOKEN));
-refresh_token(Appid,REFRESH_TOKEN) when is_list(Appid),is_list(REFRESH_TOKEN) ->
+  ResponseJson = jsx:decode(list_to_binary(Response)),
+  Access_token=proplists:get_value(<<"access_token">>,ResponseJson),
+  Access_token.
+get_access_token(Code) when is_binary(Code)->
+  get_access_token(binary_to_list(Code));
+get_access_token(Code) when is_list(Code)->
+  Appid = get_apppid(),
+  Secret=get_secret(),
   %Access_token_Rui = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="++Appid++"&secret="++Secret++"&code="++binary_to_list(Code)++"&grant_type=authorization_code",
-  REFRESH_TOKEN_Rui = ?API_URL_REFRESH_TOKEN++"?appid="++Appid++"&grant_type=refresh_token&refresh_token="++REFRESH_TOKEN,
+  Access_token_Rui = ?API_URL_ACCESS_TOKEN++"?appid="++Appid++"&secret="++Secret++"&code="++Code++"&grant_type=authorization_code",
+  Response= wechat_util:http_get(Access_token_Rui),
+  Response_Json = jsx:decode(list_to_binary(Response)),
+  Response_Json.
+refresh_token(Reftesh_token) when is_binary(Reftesh_token)->
+  Appid = get_apppid(),
+  refresh_token(binary_to_list(Appid),binary_to_list(Reftesh_token)).
+refresh_token(Appid,Reftesh_token) when is_list(Appid),is_list(Reftesh_token) ->
+  %Access_token_Rui = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="++Appid++"&secret="++Secret++"&code="++binary_to_list(Code)++"&grant_type=authorization_code",
+  REFRESH_TOKEN_Rui = ?API_URL_REFRESH_TOKEN++"?appid="++Appid++"&grant_type=refresh_token&refresh_token="++Reftesh_token,
   Response= wechat_util:http_get(REFRESH_TOKEN_Rui),
-  Response.
-
-
-get_userinfo(ACCESS_TOKEN,OPENID) when is_binary(ACCESS_TOKEN),is_binary(OPENID)->
-  get_userinfo(binary_to_list(ACCESS_TOKEN),binary_to_list(OPENID));
-get_userinfo(ACCESS_TOKEN,OPENID) when is_list(ACCESS_TOKEN),is_list(OPENID)->
+  Response_Json = jsx:decode(list_to_binary(Response)),
+  Response_Json.
+get_userinfo(Openid) when is_binary(Openid)->
+  Access_token = env_access_token(),
+  get_userinfo(binary_to_list(Access_token),binary_to_list(Openid)).
+get_userinfo(Access_token,Openid) when is_list(Access_token),is_list(Openid)->
   %Userinfo_Url = "https://api.weixin.qq.com/sns/userinfo?access_token="++binary_to_list(ACCESS_TOKEN)++"&openid="++binary_to_list(OPENID),
-  Userinfo_Url =?API_URL_USERINFO++"?access_token="++ACCESS_TOKEN++"&openid="++OPENID++"&lang=zh_CN",
+  Userinfo_Url =?API_URL_USERINFO++"?access_token="++Access_token++"&openid="++Openid++"&lang=zh_CN",
   Userinfo= wechat_util:http_get(Userinfo_Url),
-  Userinfo.
-
-check(Signature, Timestamp, Nonce,Token) when is_binary(Signature),is_binary(Timestamp),is_binary(Nonce),is_binary(Token) ->
-  check(binary_to_list(Signature), binary_to_list(Timestamp), binary_to_list(Nonce),binary_to_list(Token));
-check(Signature, Timestamp, Nonce,Token) when is_list(Signature),is_list(Timestamp),is_list(Nonce),is_list(Token)->
+  Response_Json = jsx:decode(list_to_binary(Userinfo)),
+  Response_Json.
+verify(Signature, Timestamp, Nonce,Token) when is_binary(Signature),is_binary(Timestamp),is_binary(Nonce),is_binary(Token) ->
+  verify(binary_to_list(Signature), binary_to_list(Timestamp), binary_to_list(Nonce),binary_to_list(Token));
+verify(Signature, Timestamp, Nonce,Token) when is_list(Signature),is_list(Timestamp),is_list(Nonce),is_list(Token)->
   TmpList = [Token, Timestamp, Nonce],
   TmpList2 = lists:sort(TmpList),
   TmpStr = string:join(TmpList2, ""),
@@ -64,14 +120,23 @@ send_template_message(AccessToken, Message) when is_list(AccessToken),is_binary(
 
 
 check_test()->
-  Check = check(<<"95a0d735a1b79c3f23054f2dd1068884f654ea4a">>,<<"1492407520">>,<<"1056732191">>,<<"wechat">>),
+  Check = verify(<<"95a0d735a1b79c3f23054f2dd1068884f654ea4a">>,<<"1492407520">>,<<"1056732191">>,<<"wechat">>),
   ?assertEqual(true,Check),
   ok.
 
-get_code_url_test()->
-  Code_url=get_code_url("wxa385560bcf4ffdd1"
-    ,"http://127.0.0.1:8081/wechat/callback","123456"),
-  Expected = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa385560bcf4ffdd1&redirect_uri=http%3a%2f%2f127.0.0.1%3a8081%2fwechat%2fcallback&response_type=code&scope=snsapi_userinfo&state=123456#wechat_redirect",
-  ?assertEqual(Expected,Code_url),
-  ok.
+get_apppid() ->
+  {ok,Appid}=application:get_env(erlchat,appid),
+  Appid.
+
+get_redirect_uri() ->
+  {ok,Redirect_uri} = application:get_env(erlchat,redirect_uri),
+  Redirect_uri.
+
+get_secret() ->
+  {ok,Secret} = application:get_env(erlchat,secret),
+  Secret.
+
+env_access_token() ->
+  {ok,Access_token} = application:get_env(erlchat,access_token),
+  Access_token.
 
