9a62181112ab8727e425162927fad4ba0ebd77ea
jiarenjie <164977873@qq.com>
2017-05-24 15:08:34 +0800

    s

--- src/erlchat.erl
+++ src/erlchat.erl
@@ -64,13 +64,15 @@ get_code_url() ->
   Enc_url=edoc_lib:escape_uri(Rerirect_url),
   <<A:32,_B:32,_C:32>> = crypto:strong_rand_bytes(12),
   State = integer_to_list(A),
-  Url = ?API_URL_CODE ++ "?appid=" ++ Appid ++ "&redirect_uri=" ++ Enc_url ++ "&response_type=code&scope=snsapi_userinfo&state=" ++ State ++ "#wechat_redirect",
+  Url = ?API_URL_CODE ++ "?appid=" ++ Appid ++
+    "&redirect_uri=" ++ Enc_url ++ "&response_type=code&scope=snsapi_userinfo&state=" ++
+    State ++ "#wechat_redirect",
   Url.
 get_access_token()->
   Appid = get_apppid(),
   Secret=get_secret(),
-  %Access_token_Rui = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="++Appid++"&secret="++Secret++"&code="++binary_to_list(Code)++"&grant_type=authorization_code",
-  Access_token_Rui = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&"++"appid="++Appid++"&secret="++Secret,
+  Access_token_Rui = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&"
+    ++"appid="++Appid++"&secret="++Secret,
   Response= wechat_util:http_get(Access_token_Rui),
   ResponseJson = jsx:decode(list_to_binary(Response)),
   Access_token=proplists:get_value(<<"access_token">>,ResponseJson),
@@ -80,8 +82,8 @@ get_access_token(Code) when is_binary(Code)->
 get_access_token(Code) when is_list(Code)->
   Appid = get_apppid(),
   Secret=get_secret(),
-  %Access_token_Rui = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="++Appid++"&secret="++Secret++"&code="++binary_to_list(Code)++"&grant_type=authorization_code",
-  Access_token_Rui = ?API_URL_ACCESS_TOKEN++"?appid="++Appid++"&secret="++Secret++"&code="++Code++"&grant_type=authorization_code",
+  Access_token_Rui = ?API_URL_ACCESS_TOKEN++"?appid="++Appid++"&secret="++Secret++
+    "&code="++Code++"&grant_type=authorization_code",
   Response= wechat_util:http_get(Access_token_Rui),
   Response_Json = jsx:decode(list_to_binary(Response)),
   Response_Json.
@@ -89,8 +91,8 @@ refresh_token(Reftesh_token) when is_binary(Reftesh_token)->
   Appid = get_apppid(),
   refresh_token(binary_to_list(Appid),binary_to_list(Reftesh_token)).
 refresh_token(Appid,Reftesh_token) when is_list(Appid),is_list(Reftesh_token) ->
-  %Access_token_Rui = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="++Appid++"&secret="++Secret++"&code="++binary_to_list(Code)++"&grant_type=authorization_code",
-  REFRESH_TOKEN_Rui = ?API_URL_REFRESH_TOKEN++"?appid="++Appid++"&grant_type=refresh_token&refresh_token="++Reftesh_token,
+  REFRESH_TOKEN_Rui = ?API_URL_REFRESH_TOKEN++"?appid="++Appid++
+    "&grant_type=refresh_token&refresh_token="++Reftesh_token,
   Response= wechat_util:http_get(REFRESH_TOKEN_Rui),
   Response_Json = jsx:decode(list_to_binary(Response)),
   Response_Json.
@@ -98,13 +100,14 @@ get_userinfo(Openid) when is_binary(Openid)->
   Access_token = env_access_token(),
   get_userinfo(binary_to_list(Access_token),binary_to_list(Openid)).
 get_userinfo(Access_token,Openid) when is_list(Access_token),is_list(Openid)->
-  %Userinfo_Url = "https://api.weixin.qq.com/sns/userinfo?access_token="++binary_to_list(ACCESS_TOKEN)++"&openid="++binary_to_list(OPENID),
   Userinfo_Url =?API_URL_USERINFO++"?access_token="++Access_token++"&openid="++Openid++"&lang=zh_CN",
   Userinfo= wechat_util:http_get(Userinfo_Url),
   Response_Json = jsx:decode(list_to_binary(Userinfo)),
   Response_Json.
-verify(Signature, Timestamp, Nonce,Token) when is_binary(Signature),is_binary(Timestamp),is_binary(Nonce),is_binary(Token) ->
-  verify(binary_to_list(Signature), binary_to_list(Timestamp), binary_to_list(Nonce),binary_to_list(Token));
+verify(Signature, Timestamp, Nonce,Token) when
+  is_binary(Signature),is_binary(Timestamp),
+  is_binary(Nonce),is_binary(Token) ->
+    verify(binary_to_list(Signature), binary_to_list(Timestamp), binary_to_list(Nonce),binary_to_list(Token));
 verify(Signature, Timestamp, Nonce,Token) when is_list(Signature),is_list(Timestamp),is_list(Nonce),is_list(Token)->
   TmpList = [Token, Timestamp, Nonce],
   TmpList2 = lists:sort(TmpList),
